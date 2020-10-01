64a0f0cc135c1bb2aa66eb1d6d6fe6d88d49824b
Luís Filipe <l.filipesartori@gmail.com>
2020-10-01 14:58:25 -0300

    Adicionando contatos na lista com ng-model e ng-click

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ angular.module("listaTelefonica", []);
 angular.module("listaTelefonica").controller("listaTelefonicaController", function($scope) {
     $scope.app = "Lista Telefonica";
     $scope.contatos = [
-        {nome: "Pedro", telefone: "99998888"},
-        {nome: "Ana", telefone: "99998877"},
-        {nome: "Maria", telefone: "99998866"},
+        {nome: "João", telefone: "88889999"},
+        {nome: "Ana", telefone: "88888888"},
+        {nome: "Pedro", telefone: "88887777"},
     ];
+   $scope.adicionarContato = function (contato) {
+        $scope.contatos.push(angular.copy(contato));
+        delete $scope.contato;
+   }
 });
+
