540816b42f16cc3714c536152c6c19970e2bac22
Luís Filipe <l.filipesartori@gmail.com>
2020-10-02 16:22:20 -0300

    Incluindo nome, telefone, select de operadoras e exclusao de contatos

--- app.js
+++ app.js
@@ -2,13 +2,31 @@ angular.module("listaTelefonica", []);
 angular.module("listaTelefonica").controller("listaTelefonicaController", function($scope) {
     $scope.app = "Lista Telefonica";
     $scope.contatos = [
-        {nome: "João", telefone: "88889999"},
-        {nome: "Ana", telefone: "88888888"},
-        {nome: "Pedro", telefone: "88887777"},
+        {nome: "João", telefone: "88889999", cor: "blue"},
+        {nome: "Ana", telefone: "88888888", cor: "red"},
+        {nome: "Pedro", telefone: "88887777", cor: "purple"},
+    ];
+    $scope.operadoras = [
+        {nome: "Oi", codigo: "14", categoria: "Celular"},
+        {nome: "Vivo", codigo: "15", categoria: "Celular"},
+        {nome: "Tim", codigo: "41", categoria: "Celular"},
+        {nome: "GVT", codigo: "25", categoria: "Fixo"},
+        {nome: "Embratel", codigo: "21", categoria: "Fixo"},
     ];
    $scope.adicionarContato = function (contato) {
         $scope.contatos.push(angular.copy(contato));
         delete $scope.contato;
-   }
+   };
+   $scope.apagarContatos = function (contatos) {
+       $scope.contatos =  contatos.filter(function(contato){
+            if (!contato.selecionado) return contato;
+        });
+    };
+    $scope.isContatoSelecionado = function (contatos) {
+        return contatos.some(function (contato) {
+            return contato.selecionado;
+        });
+    };
+
 });
 
