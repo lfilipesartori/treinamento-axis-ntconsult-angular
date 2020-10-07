1937e24b3fa2fef42f97975e271fc03bb48224dd
Luís Filipe <l.filipesartori@gmail.com>
2020-10-07 15:20:29 -0300

    Adicionando filters e campo de busca

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ angular.module("listaTelefonica", ["ngMessages"]);
 angular.module("listaTelefonica").controller("listaTelefonicaController", function($scope) {
     $scope.app = "Lista Telefonica";
     $scope.contatos = [
-        {nome: "João", telefone: "88889999", cor: "blue"},
-        {nome: "Ana", telefone: "88888888", cor: "red"},
-        {nome: "Pedro", telefone: "88887777", cor: "purple"},
+        {nome: "João", telefone: "88889999", data: new Date(), operadora: {nome: "Oi", codigo: "14", categoria: "Celular"}},
+        {nome: "Ana", telefone: "88888888", data: new Date(), operadora: {nome: "Vivo", codigo: "15", categoria: "Celular"}},
+        {nome: "Pedro", telefone: "88887777", data: new Date(), operadora: {nome: "Tim", codigo: "41", categoria: "Celular"}},
     ];
     $scope.operadoras = [
-        {nome: "Oi", codigo: "14", categoria: "Celular"},
-        {nome: "Vivo", codigo: "15", categoria: "Celular"},
-        {nome: "Tim", codigo: "41", categoria: "Celular"},
-        {nome: "GVT", codigo: "25", categoria: "Fixo"},
-        {nome: "Embratel", codigo: "21", categoria: "Fixo"},
+        {nome: "Oi", codigo: "14", categoria: "Celular", preco: 2},
+        {nome: "Vivo", codigo: "15", categoria: "Celular", preco: 1},
+        {nome: "Tim", codigo: "41", categoria: "Celular", preco: 3},
+        {nome: "GVT", codigo: "25", categoria: "Fixo", preco: 1},
+        {nome: "Embratel", codigo: "21", categoria: "Fixo", preco: 2},
     ];
    $scope.adicionarContato = function (contato) {
         $scope.contatos.push(angular.copy(contato));
@@ -27,6 +27,10 @@ angular.module("listaTelefonica").controller("listaTelefonicaController", functi
         return contatos.some(function (contato) {
             return contato.selecionado;
         });
+    };
+    $scope.ordenarPor = function (campo){
+        $scope.criterioDeOrdenacao = campo;
+        $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
     };
 
 });
